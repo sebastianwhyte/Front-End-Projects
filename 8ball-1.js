6bbb9c3507a3e5a36f4235c60b2f6557f13c7dcc
sebastianwhyte <62267311+sebastianwhyte@users.noreply.github.com>
2020-07-15 20:56:01 -0400

    Add files via upload
    Javascript magic 8 ball

---
+++ 8ball-1.js
@@ -0,0 +1,36 @@
+var button = document.getElementById("button");
+var input = document.getElementById("input");
+var answer = document.getElementById("answer");
+var numberEight = document.getElementById("number-eight");
+var options = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes – definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Don’t count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+];
+
+button.addEventListener("click", function () {
+  if (input.value.length < 1) {
+    alert("Please enter a question first.");
+  } else {
+    eight.innerText = "";
+    var num = input.value.length % options.length;
+    answer.innerText = options[num];
+  }
+});
